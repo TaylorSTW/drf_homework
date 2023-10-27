d05c464a2d5c6bf5c33b5ed02a0a89877bc88ba4
TaylorSTW <dmitfrost@ya.ru>
2023-10-27 20:56:36 +0300

    27.2 hm

--- course/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class MyPagination(PageNumberPagination):
-
-    page_size = 2
-    page_query_param = 'page'
-    max_page_size = 100
